743a159d1afc542b4412e1de5733296ca6b73389
Shereif <chachushereef@gmail.com>
2021-06-27 10:52:47 +0800

    adding new deps

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,36 +1,20 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 module.exports = {
   mode: "development",
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
     ],
   },
-  resolve: {
-    extensions: [".js", ".jsx"],
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+
+  devtool: false,
   devServer: {
-    historyApiFallback: true,
-  },
-  externals: {
-    // global app config object
-    config: JSON.stringify({
-      apiUrl: "http://localhost:4000",
-    }),
+    contentBase: "./dist",
   },
 };
