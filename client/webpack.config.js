aca53b46f5752b5a8b9cb7592319c2769a5d42d4
Shereif <chachushereef@gmail.com>
2021-06-27 13:10:15 +0800

    adding more config webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 let mode = process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
   mode: mode,
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
 
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
       {
         test: /\.(s[ac]|c)ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
@@ -19,12 +33,19 @@ module.exports = {
     ],
   },
 
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
   resolve: {
     extensions: [".js", ".jsx"],
   },
 
   devtool: "source-map",
   devServer: {
-    contentBase: "./public",
+    contentBase: "./dist",
   },
 };
