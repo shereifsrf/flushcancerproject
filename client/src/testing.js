97d568cfdf35c071d27b338f8deaabd92fc5c330
Shereif <chachushereef@gmail.com>
2021-07-04 16:54:33 +0800

    test netlify tjs 4

--- client/src/testing.js
+++ client/src/testing.js
@@ -2,13 +2,5 @@ import React from "react";
 
 export default function AlertDialog(props) {
   const mode = process.env.NODE_ENV;
-  const url = process.env.REACT_APP_SERVER_URL;
-  const urlL = process.env.REACT_APP_LOCAL_URL;
-  return (
-    <>
-      {mode}
-      <br /> {url}
-      <br /> {urlL}
-    </>
-  );
+  return <>{mode}</>;
 }
