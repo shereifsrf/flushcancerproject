76671da07e54f98333ade98959053e867144db5f
Shereif <chachushereef@gmail.com>
2021-07-04 15:44:35 +0800

    test frikking netlify

--- client/src/testing.js
+++ client/src/testing.js
@@ -1,8 +1,9 @@
 import React from "react";
+
 const mode = process.env.NODE_ENV;
 const url = process.env.SERVER_URL;
 const urlL = process.env.LOCAL_URL;
 
 export default function AlertDialog(props) {
-  return <>{url}</>;
+  return <>{{ mode, urlL, url }}</>;
 }
