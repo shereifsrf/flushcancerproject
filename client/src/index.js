7580f066a0d02eb55459e5bfc60858fa42b1fb16
Shereif <chachushereef@gmail.com>
2021-07-04 15:49:45 +0800

    test frikking netlify 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
-import App from "./testing";
+import App from "./App";
+// import App from "./testing";
 
 ReactDOM.render(<App />, document.getElementById("root"));
